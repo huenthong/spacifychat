90b0f5f9e2c0bf54e8ba726e0ad6a6794219cd28
Huenthong_ <126305082+huenthong@users.noreply.github.com>
2025-08-21 10:05:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -452,7 +452,7 @@ st.markdown("""
 }
 
 .metric-card {
-    background-color: white;
+    background-color: #fff4e0;
     padding: 20px;
     border-radius: 10px;
     box-shadow: 0 2px 4px rgba(0,0,0,0.1);
